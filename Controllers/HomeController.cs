1dff0dd5657d0f82cd0354a71c4f575340914379
qingxue0606 <77055664@qq.com>
2020-05-19 11:54:52 +0800

    first
    Signed-off-by: qingxue0606 <77055664@qq.com>

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Logging;
+using POWord.Models;
+
+namespace POWord.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private String connString;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment webHostEnvironment)
+        {
+            _logger = logger;
+            _webHostEnvironment = webHostEnvironment;
+            string rootPath = _webHostEnvironment.WebRootPath.Replace("/", "\\");
+            string dataPath = rootPath.Substring(0, rootPath.Length - 7) + "AppData\\" + "demo_poword.db";
+            connString = "Data Source=" + dataPath;
+        }
+
+        public IActionResult Index()
+        {
+            string sql = "select * from leaveRecord order by ID DESC ";
+            SqliteConnection conn = new SqliteConnection(connString);
+
+            conn.Open();
+            SqliteCommand cmd = new SqliteCommand(sql, conn);
+            cmd.ExecuteNonQuery();
+            cmd.CommandText = sql;
+            SqliteDataReader dr = cmd.ExecuteReader();
+            StringBuilder strGrid = new StringBuilder();
+
+            if (!dr.HasRows)
+            {
+                strGrid.Append("<tr class='XYDataGrid1-table-data-tr'>\r\n");
+                strGrid.Append("<td colspan=4 width='100%' height='100' class='XYDataGrid1-data-cell' align='center'>对不起，暂时没有可以操作的文档。\r\n");
+                strGrid.Append("</td></tr>\r\n");
+            }
+            else
+            {
+                while (dr.Read())
+                {
+                    strGrid.Append("<tr onmouseover='onColor(this)' onmouseout='offColor(this)' class='XYDataGrid1-table-data-tr'>\r\n");
+                    strGrid.Append("<td width='7%' height='16' bgcolor='' class='XYDataGrid1-data-cell'><div align='center'><image src='images/word.gif' border='0'></image></div></td>\r\n");
+                    strGrid.Append("<td width='28%' height='16' bgcolor='' class='XYDataGrid1-data-cell'><div align='left'>" + dr["Subject"].ToString() + "</div></td>\r\n");
+                    strGrid.Append("<td width='20%' height='16' bgcolor='' class='XYDataGrid1-data-cell'><div align='center'><font face='宋体'>" + dr["SubmitTime"].ToString() + "</font></div></td>\r\n");
+                    strGrid.Append("<td width='45%' height='16' bgcolor='' class='XYDataGrid1-data-cell'><div align='center'>\r\n");
+                    strGrid.Append("<a class=OPLink href='Word/datalist?ID=" + dr["ID"].ToString() + "'  target='_blank' >数据库中字段内容</a>&nbsp;\r\n");
+                    strGrid.Append("<a class=OPLink href=\"javascript:POBrowser.openWindow('Word/SubmitDataOfDoc?ID=" + dr["ID"].ToString() + "', 'width=1200px;height=800px;');\">用户填写请假条</a>&nbsp;\r\n");
+                    strGrid.Append("<a class=OPLink href=\"javascript:POBrowser.openWindow('Word/GenDoc?ID=" + dr["ID"].ToString() + "', 'width=1200px;height=800px;');\">动态生成格式文档</a>&nbsp;\r\n");
+                    strGrid.Append("</div></td></tr>\r\n");
+                }
+            }
+            dr.Close();
+            conn.Close();
+
+            ViewBag.strGrid = strGrid;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Controllers/WordController.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
+
+namespace POWord.Controllers
+{
+    public class WordController : Controller
+    {
+        private string connString;
+
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public WordController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+            string dataPath = _webHostEnvironment.WebRootPath.Replace("/", "\\");
+            dataPath = dataPath.Substring(0, dataPath.Length - 7) + "appData\\" + "demo_poword.db";
+            connString = "Data Source=" + dataPath;
+        }
+        public IActionResult datalist()
+        {
+
+            string docID = Request.Query["ID"];
+
+            string sql = "select * from leaveRecord where ID = " + docID;
+            SqliteConnection conn = new SqliteConnection(connString);
+
+            conn.Open();
+            SqliteCommand cmd = new SqliteCommand(sql, conn);
+            cmd.ExecuteNonQuery();
+            cmd.CommandText = sql;
+            SqliteDataReader dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                //docFile = Reader["FileName"].ToString();
+                ViewBag.Subject = dr["Subject"].ToString();
+                ViewBag.Name = dr["Name"].ToString();
+                ViewBag.Dept = dr["Dept"].ToString();
+                ViewBag.Cause = dr["Cause"].ToString();
+                ViewBag.Num = dr["Num"].ToString();
+                ViewBag.SubmitTime = dr["SubmitTime"].ToString();
+            }
+            dr.Close();
+            conn.Close();
+            return View();
+        }
+
+         public IActionResult GenDoc()
+        {
+
+
+            string docID = Request.Query["ID"];
+
+            string sql = "select * from leaveRecord where ID = " + docID;
+            SqliteConnection conn = new SqliteConnection(connString);
+
+            conn.Open();
+            SqliteCommand cmd = new SqliteCommand(sql, conn);
+            cmd.ExecuteNonQuery();
+            cmd.CommandText = sql;
+            SqliteDataReader dr = cmd.ExecuteReader();
+
+            PageOfficeNetCore.PageOfficeCtrl pageofficeCtrl = new PageOfficeNetCore.PageOfficeCtrl(Request);
+            pageofficeCtrl.ServerPage = "../PageOffice/POServer";
+            PageOfficeNetCore.WordWriter.WordDocument doc = new PageOfficeNetCore.WordWriter.WordDocument();
+            doc.DisableWindowRightClick = true;
+            if (dr.Read())
+            {
+
+                doc.OpenDataRegion("PO_name").Value = dr["Name"].ToString();
+                doc.OpenDataRegion("PO_dept").Value = dr["Dept"].ToString();
+                doc.OpenDataRegion("PO_cause").Value = dr["Cause"].ToString(); 
+                doc.OpenDataRegion("PO_num").Value = dr["Num"].ToString(); ;
+                doc.OpenDataRegion("PO_date").Value = dr["SubmitTime"].ToString(); ;
+                doc.OpenDataRegion("PO_tip").Value = ""; ;
+
+            }
+            dr.Close();
+            conn.Close();
+
+            // 设置界面样式
+            pageofficeCtrl.Caption = "动态生成格式文件";
+            pageofficeCtrl.BorderStyle = PageOfficeNetCore.BorderStyleType.BorderThin;
+            // 添加自定义工具条按钮
+            pageofficeCtrl.AddCustomToolButton("打印", "poPrint", 6);
+            pageofficeCtrl.AddCustomToolButton("全屏/还原", "poSetFullScreen", 4);
+
+            ////获取数据对象
+            pageofficeCtrl.SetWriter(doc);
+
+
+            //打开Word文档
+            pageofficeCtrl.WebOpen("/doc/template.doc", PageOfficeNetCore.OpenModeType.docReadOnly, "tom");
+            ViewBag.POCtrl = pageofficeCtrl.GetHtmlCode("PageOfficeCtrl1");
+
+            return View();
+        }
+
+        public IActionResult SubmitDataOfDoc()
+        {
+
+            string docID = Request.Query["ID"];
+
+            string sql = "select * from leaveRecord where ID = " + docID;
+            SqliteConnection conn = new SqliteConnection(connString);
+
+            conn.Open();
+            SqliteCommand cmd = new SqliteCommand(sql, conn);
+            cmd.ExecuteNonQuery();
+            cmd.CommandText = sql;
+            SqliteDataReader dr = cmd.ExecuteReader();
+
+            PageOfficeNetCore.PageOfficeCtrl pageofficeCtrl = new PageOfficeNetCore.PageOfficeCtrl(Request);
+            pageofficeCtrl.ServerPage = "../PageOffice/POServer";
+            PageOfficeNetCore.WordWriter.WordDocument doc = new PageOfficeNetCore.WordWriter.WordDocument();
+            PageOfficeNetCore.WordWriter.DataRegion drName = doc.OpenDataRegion("PO_name");
+
+            drName.Editing = true;
+            PageOfficeNetCore.WordWriter.DataRegion drDept = doc.OpenDataRegion("PO_dept");
+
+            drDept.Shading.BackgroundPatternColor = Color.Silver;
+            //drDept.Editing = true;
+            PageOfficeNetCore.WordWriter.DataRegion drCause = doc.OpenDataRegion("PO_cause");
+   
+            drCause.Editing = true;
+            PageOfficeNetCore.WordWriter.DataRegion drNum = doc.OpenDataRegion("PO_num");
+        
+            drNum.Editing = true;
+            PageOfficeNetCore.WordWriter.DataRegion drDate = doc.OpenDataRegion("PO_date");
+            drDate.Shading.BackgroundPatternColor = Color.Pink;
+            //drDate.Editing = true;
+            PageOfficeNetCore.WordWriter.DataRegion drTip = doc.OpenDataRegion("PO_tip");
+            drTip.Font.Italic = true;
+            drTip.Value = "提示：带背景色的文字是只能通过选择设置，[]中的文字是可以录入编辑的。";
+
+
+
+            if (dr.Read())
+            {
+                doc.OpenDataRegion("PO_name").Value = dr["Name"].ToString();
+                doc.OpenDataRegion("PO_dept").Value = dr["Dept"].ToString();
+                doc.OpenDataRegion("PO_cause").Value = dr["Cause"].ToString();
+                doc.OpenDataRegion("PO_num").Value = dr["Num"].ToString(); ;
+                doc.OpenDataRegion("PO_date").Value = dr["SubmitTime"].ToString(); ;
+            }
+            dr.Close();
+            conn.Close();
+
+
+            // 设置界面样式
+            pageofficeCtrl.Caption = "用户填写请假条";
+            pageofficeCtrl.BorderStyle = PageOfficeNetCore.BorderStyleType.BorderThin;
+            // 添加自定义工具条按钮
+            pageofficeCtrl.AddCustomToolButton("保存", "poSave", 1);
+            pageofficeCtrl.AddCustomToolButton("全屏/还原", "poSetFullScreen", 4);
+
+            pageofficeCtrl.JsFunction_OnWordDataRegionClick = "OnWordDataRegionClick()";
+
+            //获取数据对象
+            pageofficeCtrl.SetWriter(doc);
+
+            //设置保存页面
+            pageofficeCtrl.SaveDataPage = "SaveData?ID=" + docID;
+            //打开Word文档
+            pageofficeCtrl.WebOpen("/doc/template.doc", PageOfficeNetCore.OpenModeType.docSubmitForm, "tom");
+            ViewBag.POCtrl = pageofficeCtrl.GetHtmlCode("PageOfficeCtrl1");
+
+            return View();
+        }
+
+        public async Task<ActionResult> SaveData() {
+
+            string ErrorMsg = "";
+            PageOfficeNetCore.WordReader.WordDocument doc = new PageOfficeNetCore.WordReader.WordDocument(Request, Response);
+            await doc.LoadAsync();
+            
+            string sName = doc.OpenDataRegion("PO_name").Value;
+            string sDept = doc.OpenDataRegion("PO_dept").Value;
+            string sCause = doc.OpenDataRegion("PO_cause").Value;
+            string sNum = doc.OpenDataRegion("PO_num").Value;
+            string sDate = doc.OpenDataRegion("PO_date").Value;
+
+            if (sName == "")
+            {
+                ErrorMsg = ErrorMsg + "<li>申请人</li>";
+            }
+            if (sDept == "")
+            {
+                ErrorMsg = ErrorMsg + "<li>部门名称</li>";
+            }
+            if (sCause == "")
+            {
+                ErrorMsg = ErrorMsg + "<li>请假原因</li>";
+            }
+            if (sDate == "")
+            {
+                ErrorMsg = ErrorMsg + "<li>日期</li>";
+            }
+
+            try
+            {
+                if (sNum != "")
+                {
+                    if (Int64.Parse(sNum) < 0)
+                    {
+                        ErrorMsg = ErrorMsg + "<li>请假天数不能是负数</li>";
+                    }
+                }
+                else
+                {
+                    ErrorMsg = ErrorMsg + "<li>请假天数</li>";
+                }
+            }
+            catch (Exception Ex)
+            {
+                ErrorMsg = ErrorMsg + "<li><font color=red>注意：</font>请假天数必须是数字</li>";
+            }
+
+            string id = Request.Query["ID"];
+            string sql = "";
+            if (ErrorMsg == "")
+            {
+                sql = "update leaveRecord set Name='" + sName
+                + "', Dept='" + sDept + "', Cause='" + sCause
+                + "', Num=" + sNum + ", SubmitTime='" + sDate
+                + "' where  ID=" + id;
+                SqliteConnection conn = new SqliteConnection(connString);
+                conn.Open();
+                SqliteCommand cmd = new SqliteCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+            else
+            {
+                await Response.Body.WriteAsync(Encoding.GetEncoding("GB2312").GetBytes(ErrorMsg));
+                doc.ShowPage(578, 380);
+            }
+            doc.Close();
+            return Content("OK");
+        }
+
+    }
+}
